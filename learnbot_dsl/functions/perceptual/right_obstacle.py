1624398904286f09f74232ac7e88b5563df1e989
ibarbech <ibarbech@alumnos.unex.es>
2018-02-01 23:20:34 +0100

    create git annex

--- learnbot_dsl/functions/perceptual/right_obstacle.py
+++
@@ -1,9 +0,0 @@
-def right_obstacle(lbot, threshold= 200, verbose=False):
-	sonarsValue = lbot.getSonars()
-	if sonarsValue['right'] < threshold:
-		if verbose:
-			print('Obstacle right of Learnbot')
-		return True
-	if verbose:
-		print('No obstacle right of Learnbot')
-	return False
